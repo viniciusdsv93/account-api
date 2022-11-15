ed4378646e76f522d811a04a7a17a3d2eac22f45
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 12:45:41 -0300

    test: ensure UserPrismaRepository checks if the username is available

--- src/infra/database/postgres/user-respository/user.spec.ts
+++ src/infra/database/postgres/user-respository/user.spec.ts
@@ -1,6 +1,11 @@
+import { prismaClient } from "../prisma/prisma-client";
 import { UserPrismaRepository } from "./user";
 
 describe("User Prisma Repository", () => {
+	beforeEach(async () => {
+		await prismaClient.user.deleteMany();
+	});
+
 	test("Should return an user on success", async () => {
 		const sut = new UserPrismaRepository();
 		const createdUser = await sut.add({
@@ -10,5 +15,21 @@ describe("User Prisma Repository", () => {
 		expect(createdUser).toHaveProperty("id");
 		expect(createdUser).toHaveProperty("username");
 		expect(createdUser).toHaveProperty("password");
+	});
+
+	test("Should return true if username is available", async () => {
+		const sut = new UserPrismaRepository();
+		const isAvailable = await sut.isAvailable("test_username");
+		expect(isAvailable).toBe(true);
+	});
+
+	test("Should return false if username is unnavailable", async () => {
+		const sut = new UserPrismaRepository();
+		const createdUser = await sut.add({
+			username: "valid_username",
+			password: "Valid_password1",
+		});
+		const isAvailable = await sut.isAvailable("valid_username");
+		expect(isAvailable).toBe(false);
 	});
 });
