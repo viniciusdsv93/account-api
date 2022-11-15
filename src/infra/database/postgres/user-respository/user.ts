fdf50f757b7c9be92f2ba58c959b103a3cc51d59
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 12:45:36 -0300

    feat: ensure UserPrismaRepository checks if the username is available

--- src/infra/database/postgres/user-respository/user.ts
+++ src/infra/database/postgres/user-respository/user.ts
@@ -1,11 +1,24 @@
 import { IAddUserRepository } from "../../../../application/protocols/add-user-repository";
+import { IUsernameAvailableRepository } from "../../../../application/protocols/username-available-repository";
 import {
 	RegisterUserModel,
 	UserModelWithoutAccountId,
 } from "../../../../domain/usecases/register-user";
 import { prismaClient } from "../prisma/prisma-client";
 
-export class UserPrismaRepository implements IAddUserRepository {
+export class UserPrismaRepository
+	implements IAddUserRepository, IUsernameAvailableRepository
+{
+	async isAvailable(username: string): Promise<boolean> {
+		const existsUser = await prismaClient.user.findFirst({
+			where: {
+				username: username,
+			},
+		});
+
+		return existsUser ? false : true;
+	}
+
 	async add(userData: RegisterUserModel): Promise<UserModelWithoutAccountId> {
 		const { username, password } = userData;
 		return await prismaClient.user.create({
