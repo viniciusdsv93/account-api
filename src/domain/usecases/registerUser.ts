f85492355e1fcce4b79e13766ce0e3f5c37a2f62
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-14 21:59:55 -0300

    refactor: make registerUserUsecase return an user model without an account id

--- src/domain/usecases/registerUser.ts
+++ src/domain/usecases/registerUser.ts
@@ -1,11 +1,15 @@
-import { UserModel } from "../models/user";
-
 export type RegisterUserModel = {
 	username: string;
 	password: string;
 	passwordConfirmation: string;
 };
 
+export type UserModelWithoutAccountId = {
+	id: string;
+	username: string;
+	password: string;
+};
+
 export interface RegisterUser {
-	execute: (user: RegisterUserModel) => Promise<UserModel>;
+	execute: (user: RegisterUserModel) => Promise<UserModelWithoutAccountId>;
 }
