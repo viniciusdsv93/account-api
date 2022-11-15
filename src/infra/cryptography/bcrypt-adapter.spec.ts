91210d7a9166e2d0898271a627c2ea63c169fba0
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 11:25:07 -0300

    feat: ensure BcryptAdapter returns a hash

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -22,8 +22,9 @@ describe("Bcrypt Adapter", () => {
 		expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
 	});
 
-	// test("Should return a hash on success", () => {
-	// 	const sut = makeSut();
-	// 	const hashSpy = jest.spyOn(sut, "encrypt");
-	// });
+	test("Should return a hash on success", async () => {
+		const sut = makeSut();
+		const hash = await sut.encrypt("any_value");
+		expect(hash).toBe("hash");
+	});
 });
