d6661f0373a428d8ede92e37bf4e30ed0322ce06
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-14 21:28:44 -0300

    test: ensure SignUpController returns 400 if no username is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -1,15 +1,14 @@
 import { SignUpController } from "./signup";
 
-describe('Sign Up Controller', () => {
-
-  test('Should return 400 if no username is provided', async () => {
-    const sut = new SignUpController()
-    const httpRequest = {
-      body: {
-        password: 'any_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-  });
-})
+describe("Sign Up Controller", () => {
+	test("Should return 400 if no username is provided", async () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				password: "any_password",
+			},
+		};
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+	});
+});
