cd9c9cb4b44fdeeae4a823c3cbc4435a6f09ac28
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 11:22:40 -0300

    test: ensure BcryptAdapter calls bcrypt with the correct value

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,29 @@
+import { BcryptAdapter } from "./bcrypt-adapter";
+import bcrypt from "bcrypt";
+
+jest.mock("bcrypt", () => {
+	return {
+		async hash(): Promise<string> {
+			return await new Promise((resolve) => resolve("hash"));
+		},
+	};
+});
+
+const salt = 12;
+const makeSut = () => {
+	return new BcryptAdapter(salt);
+};
+
+describe("Bcrypt Adapter", () => {
+	test("Should call bcrypt with the correct values", async () => {
+		const sut = makeSut();
+		const hashSpy = jest.spyOn(bcrypt, "hash");
+		await sut.encrypt("any_value");
+		expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+	});
+
+	// test("Should return a hash on success", () => {
+	// 	const sut = makeSut();
+	// 	const hashSpy = jest.spyOn(sut, "encrypt");
+	// });
+});
