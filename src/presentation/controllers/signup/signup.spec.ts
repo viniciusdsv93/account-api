439c505b6413116bb42d045976fba81cf4ae725d
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-14 21:31:39 -0300

    test: ensure SignUpController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -6,6 +6,7 @@ describe("Sign Up Controller", () => {
 		const httpRequest = {
 			body: {
 				password: "any_password",
+				passwordConfirmation: "any_password",
 			},
 		};
 		const httpResponse = await sut.handle(httpRequest);
@@ -17,6 +18,19 @@ describe("Sign Up Controller", () => {
 		const httpRequest = {
 			body: {
 				username: "any_username",
+				passwordConfirmation: "any_password",
+			},
+		};
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse.statusCode).toBe(400);
+	});
+
+	test("Should return 400 if no passwordConfirmation is provided", async () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				username: "any_username",
+				password: "any_password",
 			},
 		};
 		const httpResponse = await sut.handle(httpRequest);
