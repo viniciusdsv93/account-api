c60785a93e3aa577fe6c5a6d5f172bd18861d1e7
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 10:01:15 -0300

    refactor: move duplicated code to factory helper methods

--- src/application/usecases/register-user.spec.ts
+++ src/application/usecases/register-user.spec.ts
@@ -1,20 +1,42 @@
+import { RegisterUserModel } from "../../domain/usecases/registerUser";
 import { IEncrypter } from "../protocols/encrypter";
 import { RegisterUser } from "./register-user";
 
+const makeEncrypterStub = (): IEncrypter => {
+	class EncrypterStub implements IEncrypter {
+		async encrypt(password: string): Promise<string> {
+			return await new Promise((resolve) => resolve(password));
+		}
+	}
+	return new EncrypterStub();
+};
+
+const makeFakeUserData = (): RegisterUserModel => {
+	return {
+		username: "valid_username",
+		password: "Valid_password1",
+	};
+};
+
+type SutTypes = {
+	sut: RegisterUser;
+	encrypterStub: IEncrypter;
+};
+
+const makeSut = (): SutTypes => {
+	const encrypterStub = makeEncrypterStub();
+	const sut = new RegisterUser(encrypterStub);
+	return {
+		sut,
+		encrypterStub,
+	};
+};
+
 describe("Register User Usecase", () => {
 	test("Should call Encrypter with the correct password", async () => {
-		class Encrypter implements IEncrypter {
-			async encrypt(password: string): Promise<string> {
-				return await new Promise((resolve) => resolve(password));
-			}
-		}
-		const encrypterStub = new Encrypter();
+		const { sut, encrypterStub } = makeSut();
 		const encrypterStubSpy = jest.spyOn(encrypterStub, "encrypt");
-		const sut = new RegisterUser(encrypterStub);
-		await sut.execute({
-			username: "valid_username",
-			password: "Valid_password1",
-		});
+		await sut.execute(makeFakeUserData());
 		expect(encrypterStubSpy).toHaveBeenCalledWith("Valid_password1");
 	});
 });
