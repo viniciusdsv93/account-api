cf9a0d90a31328b946780c19bfe46d6c0b484ae0
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-16 18:53:46 -0300

    feat: create unauthorized http helper method and unauthorized error

--- src/presentation/helpers/http.ts
+++ src/presentation/helpers/http.ts
@@ -1,10 +1,18 @@
 import { ServerError } from "../errors/server-error";
+import { UnauthorizedError } from "../errors/unauthorized-error";
 import { HttpResponse } from "../protocols/http";
 
 export const badRequest = (error: Error): HttpResponse => {
 	return {
 		statusCode: 400,
 		body: error,
+	};
+};
+
+export const unauthorized = (): HttpResponse => {
+	return {
+		statusCode: 401,
+		body: new UnauthorizedError(),
 	};
 };
 
