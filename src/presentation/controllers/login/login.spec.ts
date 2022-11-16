445cf6509af45cb9b7cf884afdce5e7768ff4894
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 23:55:45 -0300

    test: create a specific repository to find a user by username and apply it to LoginController

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,74 +1,81 @@
-import { IUsernameAvailableRepository } from "../../../application/protocols/username-available-repository";
+import { IFindByUsernameRepository } from "../../../application/protocols/find-by-username-repository";
+import { UserModel } from "../../../domain/models/user";
 import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest } from "../../helpers/http";
 import { HttpRequest } from "../../protocols/http";
 import { LoginController } from "./login";
 
-describe('Login Controller', () => {
-
-  const makeFakeRequest = (): HttpRequest => {
-    return {
-      body: {
-        username: 'any_username',
-        password: 'any_password'
-      }
-    }
-  }
-
-  const makeUsernameAvailableRepositoryStub = (): IUsernameAvailableRepository => {
-		class UsernameAvailableRepositoryStub implements IUsernameAvailableRepository {
-			async isAvailable(username: string): Promise<boolean> {
-				return await new Promise((resolve) => resolve(false));
-			}
-		}
-		return new UsernameAvailableRepositoryStub();
+describe("Login Controller", () => {
+	const makeFakeRequest = (): HttpRequest => {
+		return {
+			body: {
+				username: "any_username",
+				password: "any_password",
+			},
+		};
 	};
 
-  type SutTypes = {
-    sut: LoginController
-    usernameAvailableRepositoryStub: IUsernameAvailableRepository
-  }
+	const makeFindByUsernameRepositoryStub = (): IFindByUsernameRepository => {
+		class FindByUsernameRepositoryStub implements IFindByUsernameRepository {
+			async find(username: string): Promise<UserModel> {
+				return new Promise((resolve) =>
+					resolve({
+						id: "valid_id",
+						username: "valid_username",
+						password: "valid_password",
+						accountId: "valid_account_id",
+					})
+				);
+			}
+		}
+		return new FindByUsernameRepositoryStub();
+	};
 
-  const makeSut = () => {
-    const usernameAvailableRepositoryStub = makeUsernameAvailableRepositoryStub()
-    const sut = new LoginController(usernameAvailableRepositoryStub)
-    return {
-      sut,
-      usernameAvailableRepositoryStub
-    }
-  }
+	type SutTypes = {
+		sut: LoginController;
+		findByUsernameRepositoryStub: IFindByUsernameRepository;
+	};
 
-  test('Should return 400 if no username is provided', async () => {
-    const {sut} = makeSut()
-    const httpRequest = {
-      body: {
-        password: 'any_password'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('username')))
-  });
+	const makeSut = (): SutTypes => {
+		const findByUsernameRepositoryStub = makeFindByUsernameRepositoryStub();
+		const sut = new LoginController(findByUsernameRepositoryStub);
+		return {
+			sut,
+			findByUsernameRepositoryStub,
+		};
+	};
 
-  test('Should return 400 if no password is provided', async () => {
-    const {sut} = makeSut()
-    const httpRequest = {
-      body: {
-        username: 'any_username'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
-  });
+	test("Should return 400 if no username is provided", async () => {
+		const { sut } = makeSut();
+		const httpRequest = {
+			body: {
+				password: "any_password",
+			},
+		};
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse).toEqual(badRequest(new MissingParamError("username")));
+	});
 
-  test("Should return 400 if the username provided is not registered", async () => {
-		const { sut, usernameAvailableRepositoryStub } = makeSut();
-		jest.spyOn(usernameAvailableRepositoryStub, "isAvailable").mockReturnValueOnce(
-			new Promise((resolve) => resolve(true))
+	test("Should return 400 if no password is provided", async () => {
+		const { sut } = makeSut();
+		const httpRequest = {
+			body: {
+				username: "any_username",
+			},
+		};
+		const httpResponse = await sut.handle(httpRequest);
+		expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+	});
+
+	test("Should return 400 if the username provided is not registered", async () => {
+		const { sut, findByUsernameRepositoryStub } = makeSut();
+		jest.spyOn(findByUsernameRepositoryStub, "find").mockReturnValueOnce(
+			new Promise((resolve) => resolve(null))
 		);
 		const httpResponse = await sut.handle(makeFakeRequest());
 		expect(httpResponse).toEqual(
 			badRequest(new InvalidParamError("username", "invalid username or password"))
 		);
 	});
-})
+});
