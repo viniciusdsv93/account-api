a110310e054222543580693130566299580ecc29
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 23:43:11 -0300

    feat: ensure LoginController returns 400 if the username provided is not registered

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,15 +1,32 @@
+import { IUsernameAvailableRepository } from "../../../application/protocols/username-available-repository";
+import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest, ok } from "../../helpers/http";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class LoginController implements Controller {
+
+  private readonly usernameRepository: IUsernameAvailableRepository
+
+  constructor(usernameRepository: IUsernameAvailableRepository) {
+    this.usernameRepository = usernameRepository
+  }
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const requiredFields = ['username', 'password']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const {username, password} = httpRequest.body
+
+    const isAvailable = await this.usernameRepository.isAvailable(username)
+
+    if (isAvailable) {
+      return badRequest(new InvalidParamError("username", "invalid username or password"))
     }
 
     return ok('')
