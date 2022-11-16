291d49d15d0621cf10f1841a7dfe57d7c6f53915
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-15 23:43:17 -0300

    test: ensure LoginController returns 400 if the username provided is not registered

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,12 +1,46 @@
+import { IUsernameAvailableRepository } from "../../../application/protocols/username-available-repository";
+import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest } from "../../helpers/http";
+import { HttpRequest } from "../../protocols/http";
 import { LoginController } from "./login";
 
 describe('Login Controller', () => {
 
+  const makeFakeRequest = (): HttpRequest => {
+    return {
+      body: {
+        username: 'any_username',
+        password: 'any_password'
+      }
+    }
+  }
+
+  const makeUsernameAvailableRepositoryStub = (): IUsernameAvailableRepository => {
+		class UsernameAvailableRepositoryStub implements IUsernameAvailableRepository {
+			async isAvailable(username: string): Promise<boolean> {
+				return await new Promise((resolve) => resolve(false));
+			}
+		}
+		return new UsernameAvailableRepositoryStub();
+	};
+
+  type SutTypes = {
+    sut: LoginController
+    usernameAvailableRepositoryStub: IUsernameAvailableRepository
+  }
+
+  const makeSut = () => {
+    const usernameAvailableRepositoryStub = makeUsernameAvailableRepositoryStub()
+    const sut = new LoginController(usernameAvailableRepositoryStub)
+    return {
+      sut,
+      usernameAvailableRepositoryStub
+    }
+  }
 
   test('Should return 400 if no username is provided', async () => {
-    const sut = new LoginController()
+    const {sut} = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -17,7 +51,7 @@ describe('Login Controller', () => {
   });
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
+    const {sut} = makeSut()
     const httpRequest = {
       body: {
         username: 'any_username'
@@ -26,4 +60,15 @@ describe('Login Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   });
+
+  test("Should return 400 if the username provided is not registered", async () => {
+		const { sut, usernameAvailableRepositoryStub } = makeSut();
+		jest.spyOn(usernameAvailableRepositoryStub, "isAvailable").mockReturnValueOnce(
+			new Promise((resolve) => resolve(true))
+		);
+		const httpResponse = await sut.handle(makeFakeRequest());
+		expect(httpResponse).toEqual(
+			badRequest(new InvalidParamError("username", "invalid username or password"))
+		);
+	});
 })
