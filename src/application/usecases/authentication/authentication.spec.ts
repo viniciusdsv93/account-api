5f826cbce2c624d2fe3f6e3cc311bcbcc2de0742
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-17 08:24:26 -0300

    feat: ensure Authentication returns null if HashComparer returns false

--- src/application/usecases/authentication/authentication.spec.ts
+++ src/application/usecases/authentication/authentication.spec.ts
@@ -96,4 +96,13 @@ describe("Authentication UseCase", () => {
 		const promise = sut.auth(makeFakeUserData());
 		await expect(promise).rejects.toThrow();
 	});
+
+	test("Should return null if HashComparer returns false", async () => {
+		const { sut, hashComparerStub } = makeSut();
+		jest.spyOn(hashComparerStub, "compare").mockReturnValueOnce(
+			new Promise((resolve) => resolve(false))
+		);
+		const accessToken = await sut.auth(makeFakeUserData());
+		expect(accessToken).toBeNull();
+	});
 });
