058bb63a01dc8ff5943ec276500a17c67a044d43
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-17 08:20:57 -0300

    feat: ensure Authentication calls HashComparer with correct password

--- src/application/usecases/authentication/authentication.ts
+++ src/application/usecases/authentication/authentication.ts
@@ -2,18 +2,28 @@ import {
 	AuthenticationModel,
 	IAuthentication,
 } from "../../../domain/usecases/authentication";
+import { IHashComparer } from "../../protocols/cryptography/hash-comparer";
 import { IFindByUsernameRepository } from "../../protocols/repositories/find-by-username-repository";
 
 export class Authentication implements IAuthentication {
 	private readonly findByUsernameRepository: IFindByUsernameRepository;
+	private readonly hashComparer: IHashComparer;
 
-	constructor(findByUsernameRepository: IFindByUsernameRepository) {
+	constructor(
+		findByUsernameRepository: IFindByUsernameRepository,
+		hashComparer: IHashComparer
+	) {
 		this.findByUsernameRepository = findByUsernameRepository;
+		this.hashComparer = hashComparer;
 	}
 
 	async auth(authentication: AuthenticationModel): Promise<string | null> {
 		const { username, password } = authentication;
-		await this.findByUsernameRepository.find(username);
+		const user = await this.findByUsernameRepository.find(username);
+
+		if (user) {
+			await this.hashComparer.compare(password, user.password);
+		}
 
 		return null;
 	}
