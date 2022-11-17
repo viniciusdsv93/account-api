4edd466bf92451139bdd65aa7693ae205f3fe731
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-17 13:53:01 -0300

    test: ensure BcryptAdapter throws if hash throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -31,6 +31,15 @@ describe("Bcrypt Adapter", () => {
 		expect(hash).toBe("hash");
 	});
 
+	test("Should throw if hash throws", async () => {
+		const sut = makeSut();
+		jest.spyOn(bcrypt, "hash").mockImplementationOnce(
+			() => new Promise((resolve, reject) => reject(new Error()))
+		);
+		const promise = sut.hash("any_value");
+		await expect(promise).rejects.toThrow();
+	});
+
 	test("Should call compare with the correct values", async () => {
 		const sut = makeSut();
 		const compareSpy = jest.spyOn(bcrypt, "compare");
