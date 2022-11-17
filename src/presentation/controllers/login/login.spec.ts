0f1953de79e8cacac619ae89f0561951a1ec7411
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-17 07:33:01 -0300

    test: ensure LoginController returns 500 if Authentication throws

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -4,7 +4,7 @@ import { UserModel } from "../../../domain/models/user";
 import { IAuthentication } from "../../../domain/usecases/authentication";
 import { InvalidParamError } from "../../errors/invalid-param-error";
 import { MissingParamError } from "../../errors/missing-param-error";
-import { badRequest, unauthorized } from "../../helpers/http";
+import { badRequest, serverError, unauthorized } from "../../helpers/http";
 import { HttpRequest } from "../../protocols/http";
 import { LoginController } from "./login";
 
@@ -107,5 +107,14 @@ describe("Login Controller", () => {
 		);
 		const httpResponse = await sut.handle(makeFakeRequest());
 		expect(httpResponse).toEqual(unauthorized());
+	});
+
+	test("Should return 500 if Authentication throws", async () => {
+		const { sut, authenticationStub } = makeSut();
+		jest.spyOn(authenticationStub, "auth").mockReturnValueOnce(
+			new Promise((resolve, reject) => reject(new Error()))
+		);
+		const httpResponse = await sut.handle(makeFakeRequest());
+		expect(httpResponse).toEqual(serverError(new Error()));
 	});
 });
