1bd73eba3ae04a00f06c448c6c8dff08e61b3497
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 08:49:45 -0300

    refactor: delete all after tests

--- src/infra/database/postgres/account-repository/account.spec.ts
+++ src/infra/database/postgres/account-repository/account.spec.ts
@@ -8,6 +8,11 @@ describe("Account Prisma Repository", () => {
 		await prismaClient.user.deleteMany();
 	});
 
+	afterAll(async () => {
+		await prismaClient.account.deleteMany();
+		await prismaClient.user.deleteMany();
+	});
+
 	test("Should return an account on add success", async () => {
 		const sut = new AccountPrismaRepository();
 		const userSut = new UserPrismaRepository();
