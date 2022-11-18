ca20877965e47e3034917e510f5c381f2b2e2190
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 14:04:01 -0300

    feat: ensure CreateTransactionController calls CreateTransactionUseCase with correct values

--- src/presentation/controllers/create-transaction/create-transaction.ts
+++ src/presentation/controllers/create-transaction/create-transaction.ts
@@ -1,9 +1,16 @@
+import { ICreateTransaction } from "../../../domain/usecases/create-transaction";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest, ok, unauthorized } from "../../helpers/http";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class CreateTransactionController implements Controller {
+	private readonly createTransaction: ICreateTransaction;
+
+	constructor(createTransaction: ICreateTransaction) {
+		this.createTransaction = createTransaction;
+	}
+
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		const requiredFields = ["creditedUsername", "value"];
 		for (const field of requiredFields) {
@@ -15,6 +22,16 @@ export class CreateTransactionController implements Controller {
 		if (!httpRequest.headers.authorization) {
 			return unauthorized();
 		}
+
+		const { creditedUsername, value } = httpRequest.body;
+
+		const token = httpRequest.headers.authorization.split(" ")[1];
+
+		await this.createTransaction.execute({
+			token: token,
+			creditedUsername,
+			value,
+		});
 
 		return new Promise((resolve) => resolve(ok("")));
 	}
