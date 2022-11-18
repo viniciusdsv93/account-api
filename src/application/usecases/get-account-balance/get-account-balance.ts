a4760ff77eee99b9806a45ffcf2cf46ad376ee95
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 07:28:51 -0300

    feat: ensure GetAccountBalance calls decrypter with the correct token

---
+++ src/application/usecases/get-account-balance/get-account-balance.ts
@@ -0,0 +1,15 @@
+import { IGetAccountBalance } from "../../../domain/usecases/get-account-balance";
+import { IDecrypter } from "../../protocols/cryptography/decrypter";
+
+export class GetAccountBalance implements IGetAccountBalance {
+	private readonly decrypter: IDecrypter;
+
+	constructor(decrypter: IDecrypter) {
+		this.decrypter = decrypter;
+	}
+
+	async execute(token: string): Promise<number> {
+		await this.decrypter.decrypt(token);
+		return new Promise((resolve) => resolve(5));
+	}
+}
