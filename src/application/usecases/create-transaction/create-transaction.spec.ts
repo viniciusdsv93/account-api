3d1b1d758083379c7e140b07c4d5765487ee0d9b
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 18:04:16 -0300

    test: ensure CreateTransation calls Decrypter with the correct token

--- src/application/usecases/create-transaction/create-transaction.spec.ts
+++ src/application/usecases/create-transaction/create-transaction.spec.ts
@@ -1,5 +1,9 @@
 //TODO
 
+import { ICreateTransaction } from "../../../domain/usecases/create-transaction";
+import { IDecrypter } from "../../protocols/cryptography/decrypter";
+import { CreateTransaction } from "./create-transaction";
+
 //verify token
 //get debited account's userId by token
 
@@ -10,3 +14,39 @@
 // check if value is lower than balance
 
 // call repositories
+
+describe("Create Transaction UseCase", () => {
+	const makeDecrypterStub = (): IDecrypter => {
+		class DecrypterStub implements IDecrypter {
+			async decrypt(value: string): Promise<any> {
+				return new Promise((resolve) => resolve({ id: "valid_id" }));
+			}
+		}
+		return new DecrypterStub();
+	};
+
+	type SutTypes = {
+		sut: ICreateTransaction;
+		decrypterStub: IDecrypter;
+	};
+
+	const makeSut = (): SutTypes => {
+		const decrypterStub = makeDecrypterStub();
+		const sut = new CreateTransaction(decrypterStub);
+		return {
+			sut,
+			decrypterStub,
+		};
+	};
+
+	test("Should call Decrypter with the correct token", async () => {
+		const { sut, decrypterStub } = makeSut();
+		const decrypterSpy = jest.spyOn(decrypterStub, "decrypt");
+		await sut.execute({
+			token: "valid_token",
+			creditedUsername: "valid_username",
+			value: 99,
+		});
+		expect(decrypterSpy).toHaveBeenCalledWith("valid_token");
+	});
+});
