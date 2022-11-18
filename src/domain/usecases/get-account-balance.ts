01d7bce23c1540b1e525ca17540044763bcc4b60
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-17 22:37:57 -0300

    feat: ensure GetAccountBalanceController calls GetAccountBalance with correct token

--- src/domain/usecases/get-account-balance.ts
+++ src/domain/usecases/get-account-balance.ts
@@ -1,3 +1,3 @@
 export interface IGetAccountBalance {
-	getBalance(userId: string): Promise<number>;
+	execute(userId: string): Promise<number>;
 }
--- src/presentation/controllers/account-balance/get-account-balance.ts
+++ src/presentation/controllers/account-balance/get-account-balance.ts
@@ -1,13 +1,21 @@
+import { IGetAccountBalance } from "../../../domain/usecases/get-account-balance";
 import { MissingParamError } from "../../errors/missing-param-error";
 import { badRequest, ok, unauthorized } from "../../helpers/http";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class GetAccountBalanceController implements Controller {
+	private readonly getAccountBalance: IGetAccountBalance;
+
+	constructor(getAccountBalance: IGetAccountBalance) {
+		this.getAccountBalance = getAccountBalance;
+	}
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		if (!httpRequest.headers.authorization) {
 			return badRequest(new MissingParamError("token"));
 		}
+		const token = httpRequest.headers.authorization.split(" ")[1];
+		await this.getAccountBalance.execute(token);
 		return ok("");
 	}
 }
