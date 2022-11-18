935d5fb613a748c2849f007618317ae7033a8f6e
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 07:50:31 -0300

    feat: ensure GetAccountBalance calls FindAccountByUserIdRepository with the correct user id

--- src/application/usecases/get-account-balance/get-account-balance.ts
+++ src/application/usecases/get-account-balance/get-account-balance.ts
@@ -1,15 +1,26 @@
 import { IGetAccountBalance } from "../../../domain/usecases/get-account-balance";
 import { IDecrypter } from "../../protocols/cryptography/decrypter";
+import { IFindAccountByUserIdRepository } from "../../protocols/repositories/find-account-by-user-id-repository";
 
 export class GetAccountBalance implements IGetAccountBalance {
 	private readonly decrypter: IDecrypter;
+	private readonly findAccountByUserIdRepository: IFindAccountByUserIdRepository;
 
-	constructor(decrypter: IDecrypter) {
+	constructor(
+		decrypter: IDecrypter,
+		findAccountByUserIdRepository: IFindAccountByUserIdRepository
+	) {
 		this.decrypter = decrypter;
+		this.findAccountByUserIdRepository = findAccountByUserIdRepository;
 	}
 
 	async execute(token: string): Promise<number> {
 		const payload = await this.decrypter.decrypt(token);
+		if (payload) {
+			const { id } = payload;
+			await this.findAccountByUserIdRepository.findByUserId(id);
+		}
+
 		return payload;
 	}
 }
