cb7d1d18c8fc4085fbc29946eb5e3d48eae44e99
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 13:41:30 -0300

    test: ensure CreateTransactionController returns 400 if no debitedUsername is provided

---
+++ src/presentation/controllers/create-transaction/create-transaction.spec.ts
@@ -0,0 +1,18 @@
+import { MissingParamError } from "../../errors/missing-param-error";
+import { badRequest } from "../../helpers/http";
+import { CreateTransactionController } from "./create-transaction";
+
+describe("Create Transaction Controller", () => {
+	test("Should return 400 if no debitedUsername is provided", async () => {
+		const sut = new CreateTransactionController();
+		const httpResponse = await sut.handle({
+			body: {
+				creditedUsername: "any_credited_username",
+				value: 99,
+			},
+		});
+		expect(httpResponse).toEqual(
+			badRequest(new MissingParamError("debitedUsername"))
+		);
+	});
+});
