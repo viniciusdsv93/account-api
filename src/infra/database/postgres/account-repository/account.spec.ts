3f4fe35e6b905fcdb46b82f10a6fa7a20aa24ea3
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 08:21:19 -0300

    test: ensure AccountPrismaRepository returns an account on findAccountByUserId success

--- src/infra/database/postgres/account-repository/account.spec.ts
+++ src/infra/database/postgres/account-repository/account.spec.ts
@@ -8,7 +8,7 @@ describe("Account Prisma Repository", () => {
 		await prismaClient.user.deleteMany();
 	});
 
-	test("Should return an account on success", async () => {
+	test("Should return an account on add success", async () => {
 		const sut = new AccountPrismaRepository();
 		const userSut = new UserPrismaRepository();
 		const user = await userSut.add({
@@ -18,5 +18,18 @@ describe("Account Prisma Repository", () => {
 		const createdAccount = await sut.add(user.id);
 		expect(createdAccount).toHaveProperty("id");
 		expect(createdAccount).toHaveProperty("balance");
+	});
+
+	test("Should return an account on findAccountByUserId success", async () => {
+		const sut = new AccountPrismaRepository();
+		const userSut = new UserPrismaRepository();
+		const user = await userSut.add({
+			username: "valid_username2",
+			password: "Valid_password1",
+		});
+		await sut.add(user.id);
+		const accountByUserId = await sut.findByUserId(user.id);
+		expect(accountByUserId).toHaveProperty("id");
+		expect(accountByUserId).toHaveProperty("balance");
 	});
 });
