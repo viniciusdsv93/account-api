cd08cc3bcdb70c3d404bcb6798fde09023c8aeac
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 14:08:10 -0300

    comment: todos

---
+++ src/application/usecases/create-transaction/create-transaction.spec.ts
@@ -0,0 +1,12 @@
+//TODO
+
+//verify token
+//get debited account's userId by token
+
+// check if credited and debited accounts are from different people
+
+//get credited acount's userId by username
+
+// check if value is lower than balance
+
+// call repositories
