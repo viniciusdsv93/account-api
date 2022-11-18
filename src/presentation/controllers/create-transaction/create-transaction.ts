7a835e2ebf6282b124eeb5ed46f7724c44e964e9
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-18 13:41:23 -0300

    feat: ensure CreateTransactionController returns 400 if no debitedUsername is provided

---
+++ src/presentation/controllers/create-transaction/create-transaction.ts
@@ -0,0 +1,17 @@
+import { MissingParamError } from "../../errors/missing-param-error";
+import { badRequest, ok } from "../../helpers/http";
+import { Controller } from "../../protocols/controller";
+import { HttpRequest, HttpResponse } from "../../protocols/http";
+
+export class CreateTransactionController implements Controller {
+	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+		const requiredFields = ["debitedUsername"];
+		for (const field of requiredFields) {
+			if (!httpRequest.body[field]) {
+				return badRequest(new MissingParamError(field));
+			}
+		}
+
+		return new Promise((resolve) => resolve(ok("")));
+	}
+}
