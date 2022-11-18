348661de882ff895564ff4d1ee0bad117e354755
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-17 22:38:02 -0300

    test: ensure GetAccountBalanceController calls GetAccountBalance with correct token

--- src/presentation/controllers/account-balance/get-account-balance.spec.ts
+++ src/presentation/controllers/account-balance/get-account-balance.spec.ts
@@ -1,13 +1,48 @@
+import { IGetAccountBalance } from "../../../domain/usecases/get-account-balance";
 import { MissingParamError } from "../../errors/missing-param-error";
-import { badRequest, unauthorized } from "../../helpers/http";
+import { badRequest } from "../../helpers/http";
 import { GetAccountBalanceController } from "./get-account-balance";
+
+const makeGetAccountBalanceStub = (): IGetAccountBalance => {
+	class GetAccountBalanceStub implements IGetAccountBalance {
+		async execute(userId: string): Promise<number> {
+			return new Promise((resolve) => resolve(99));
+		}
+	}
+	return new GetAccountBalanceStub();
+};
+
+type SutTypes = {
+	sut: GetAccountBalanceController;
+	getAccountBalanceStub: IGetAccountBalance;
+};
+
+const makeSut = (): SutTypes => {
+	const getAccountBalanceStub = makeGetAccountBalanceStub();
+	const sut = new GetAccountBalanceController(getAccountBalanceStub);
+	return {
+		sut,
+		getAccountBalanceStub,
+	};
+};
 
 describe("Get Account Balance Controller", () => {
 	test("Should return 400 if no token is provided", async () => {
-		const sut = new GetAccountBalanceController();
+		const { sut } = makeSut();
 		const httpResponse = await sut.handle({
 			headers: {},
 		});
 		expect(httpResponse).toEqual(badRequest(new MissingParamError("token")));
+	});
+
+	test("Should call GetAccountBalanceUsecase with the correct token", async () => {
+		const { sut, getAccountBalanceStub } = makeSut();
+		const getBalanceSpy = jest.spyOn(getAccountBalanceStub, "execute");
+		await sut.handle({
+			headers: {
+				authorization: "Bearer any_token",
+			},
+		});
+		expect(getBalanceSpy).toHaveBeenCalledWith("any_token");
 	});
 });
