cf2fad57dceeb831a70314cb403c38c9874ddc46
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-19 14:36:42 -0300

    feat: include GetTransactionsController in routes

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { expressAdaptRoute } from "../adapters/expressRouteAdapter";
 import { makeCreateTransactionController } from "../factories/create-transaction";
 import { makeGetAccountBalanceController } from "../factories/get-account-balance";
+import { makeGetTransactionsController } from "../factories/get-transactions";
 import { makeLoginController } from "../factories/login";
 import { makeSignUpController } from "../factories/signup";
 
@@ -11,5 +12,6 @@ router.post("/register", expressAdaptRoute(makeSignUpController()));
 router.post("/login", expressAdaptRoute(makeLoginController()));
 router.get("/balance", expressAdaptRoute(makeGetAccountBalanceController()));
 router.post("/transaction", expressAdaptRoute(makeCreateTransactionController()));
+router.get("/transaction", expressAdaptRoute(makeGetTransactionsController()));
 
 export { router };
