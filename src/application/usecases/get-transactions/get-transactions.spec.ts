5550c006d47e9f21990b37132a7f61a974371ec1
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-19 11:32:14 -0300

    test: ensure GetTransaction returns null if Decrypter returns null

--- src/application/usecases/get-transactions/get-transactions.spec.ts
+++ src/application/usecases/get-transactions/get-transactions.spec.ts
@@ -75,14 +75,14 @@ describe("Get Account Balance UseCase", () => {
 		await expect(promise).rejects.toThrow();
 	});
 
-	// test("Should return null if Decrypter returns null", async () => {
-	// 	const { sut, decrypterStub } = makeSut();
-	// 	jest.spyOn(decrypterStub, "decrypt").mockReturnValueOnce(
-	// 		new Promise((resolve) => resolve(null))
-	// 	);
-	// 	const payload = await sut.execute("valid_token");
-	// 	expect(payload).toBeNull();
-	// });
+	test("Should return null if Decrypter returns null", async () => {
+		const { sut, decrypterStub } = makeSut();
+		jest.spyOn(decrypterStub, "decrypt").mockReturnValueOnce(
+			new Promise((resolve) => resolve(null))
+		);
+		const payload = await sut.execute("valid_token", makeFakeFilters());
+		expect(payload).toBeNull();
+	});
 
 	// test("Should return an user id on Decrypter success", async () => {
 	// 	const { decrypterStub } = makeSut();
