12e264d1c48783cbc4cec7eb2f5a2db0f8211998
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-19 15:03:30 -0300

    refactor: include query attribute on expressAdapter

--- src/main/adapters/expressRouteAdapter.ts
+++ src/main/adapters/expressRouteAdapter.ts
@@ -5,6 +5,7 @@ import { HttpRequest, HttpResponse } from "../../presentation/protocols/http";
 export const expressAdaptRoute = (controller: Controller) => {
 	return async (req: Request, res: Response) => {
 		const httpRequest: HttpRequest = {
+			query: req.query,
 			headers: req.headers,
 			body: req.body,
 		};
