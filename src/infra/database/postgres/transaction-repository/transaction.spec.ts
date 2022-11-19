99c0a464845b49efb06bbacb1795ed1e064caa52
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-19 01:03:59 -0300

    test: ensure TransactionPrismaRepository subtracts on debited account and sums on credited account on create success

--- src/infra/database/postgres/transaction-repository/transaction.spec.ts
+++ src/infra/database/postgres/transaction-repository/transaction.spec.ts
@@ -56,4 +56,27 @@ describe("Transaction Prisma Repository", () => {
 		expect(createdTransaction).toHaveProperty("value");
 		expect(createdTransaction).toHaveProperty("createdAt");
 	});
+
+	test("Should subtract on debited account and sum on credited account on create success", async () => {
+		const { sut, accountSut, userSut } = makeSut();
+		const user1 = await userSut.add({
+			username: "Orlando",
+			password: "Password1",
+		});
+		const debitedAccount = await accountSut.add(user1.id);
+		const user2 = await userSut.add({
+			username: "Mariana",
+			password: "Password2",
+		});
+		const creditedAccount = await accountSut.add(user2.id);
+		await sut.create({
+			debitedAccountId: debitedAccount.id,
+			creditedAccountId: creditedAccount.id,
+			value: 45.3,
+		});
+		const updatedDebitedAccount = await accountSut.findByUserId(user1.id);
+		const updatedCreditedAccount = await accountSut.findByUserId(user2.id);
+		expect(updatedCreditedAccount?.balance).toBe(145.3);
+		expect(updatedDebitedAccount?.balance).toBe(54.7);
+	});
 });
