61c6752a568049f013d7522a34a11d0304e15480
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-19 01:09:29 -0300

    config: insert CreateTransactionController in routes file

--- src/main/routes/routes.ts
+++ src/main/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { expressAdaptRoute } from "../adapters/expressRouteAdapter";
+import { makeCreateTransactionController } from "../factories/create-transaction";
 import { makeGetAccountBalanceController } from "../factories/get-account-balance";
 import { makeLoginController } from "../factories/login";
 import { makeSignUpController } from "../factories/signup";
@@ -9,5 +10,6 @@ const router = Router();
 router.post("/register", expressAdaptRoute(makeSignUpController()));
 router.post("/login", expressAdaptRoute(makeLoginController()));
 router.get("/balance", expressAdaptRoute(makeGetAccountBalanceController()));
+router.post("/transaction", expressAdaptRoute(makeCreateTransactionController()));
 
 export { router };
