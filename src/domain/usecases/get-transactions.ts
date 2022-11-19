a07e2de18a1cad6f715f62fbde3ebe41f7f4a258
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-19 09:01:16 -0300

    feat: create get transactions usecase interface

---
+++ src/domain/usecases/get-transactions.ts
@@ -0,0 +1,10 @@
+import { TransactionModel } from "../models/transaction";
+
+export type GetTransactionsModel = {
+	date?: string;
+	type?: "cash-in" | "cash-out";
+};
+
+export interface IGetTransactions {
+	execute(filters?: GetTransactionsModel): Promise<TransactionModel[]>;
+}
