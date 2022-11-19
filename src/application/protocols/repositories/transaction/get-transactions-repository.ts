bca975c8ef7cfa3b522a277056d5b683b3654e74
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-11-19 11:37:45 -0300

    refactor: include userId as a parameter in GetTransactionsRepository interface

--- src/application/protocols/repositories/transaction/get-transactions-repository.ts
+++ src/application/protocols/repositories/transaction/get-transactions-repository.ts
@@ -2,5 +2,8 @@ import { TransactionModel } from "../../../../domain/models/transaction";
 import { GetTransactionsModel } from "../../../../domain/usecases/get-transactions";
 
 export interface IGetTransactionsRepository {
-	get(filters: GetTransactionsModel): Promise<TransactionModel[] | null>;
+	get(
+		userId: string,
+		filters: GetTransactionsModel
+	): Promise<TransactionModel[] | null>;
 }
